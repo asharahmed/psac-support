d7dc5969006ad04d40a2068399f87a4c9e1f2e58
Ashar Ahmed <9995806+asharahmed@users.noreply.github.com>
2023-04-22 01:18:16 -0400

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -185,7 +185,7 @@ export default function Home() {
         </div>
 
         <div className="video-container">
-        <iframe   className="video" width="800vw" height="600" src="https://www.youtube.com/embed/TGjfPySSjdE" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+        <iframe   className="video" width="790vw" height="450vh" src="https://www.youtube.com/embed/TGjfPySSjdE" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
 </div>
 
 
